3a73175aa576c85ffad2b9134a4f903c1c8ab723
Faz-PC\Faz <farronflaherty@gmail.com>
2017-11-13 22:57:09 +0000

    Removing unneeded files

--- js/app.js
+++ js/app.js
@@ -2,12 +2,3 @@ function toggleMenu() {
 	var element = document.getElementById("main-menu");
     element.classList.toggle("expanded");
  };
-
- window.onscroll = function () {
-     var menu = document.getElementById('main-menu');
-     if (window.pageYOffset > 0) {
-         menu.classList.add("is-scrolled");
-     } else {
-         menu.classList.remove("is-scrolled");
-     }
- }
